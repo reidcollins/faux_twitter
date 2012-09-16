da7ce333f4956ee9c457b188ca6cc39abb4721a5
reid collins <reidcollins.2010@gmail.com>
2012-09-16 10:40:45 -0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Fauxtwitter::Application.config.secret_token = '0d5d55f09dc73ad11aa9d84599e20700068b1f3ac37f5400a1ec0e18673cfc35495bdfb071a4adfe9587afce1ee6eab19e142da2af25e14a2e0b863710300781'
